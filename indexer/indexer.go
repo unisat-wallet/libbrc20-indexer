0ad7551d6a74d3061c2ef213e4d5421c29120a25
jiedo <jiedohh@gmail.com>
2024-05-14 15:51:45 +0800

    feat: dump input after process

--- indexer/indexer.go
+++ indexer/indexer.go
@@ -32,104 +32,115 @@ func (g *BRC20ModuleIndexer) ProcessUpdateLatestBRC20Loop(brc20Datas, brc20Datas
 	}
 
 	for dataIn := range brc20Datas {
-		data := dataIn.(*model.InscriptionBRC20Data)
 
-		// is sending transfer
-		if data.IsTransfer {
-			// module conditional approve
-			if condApproveInfo, isInvalid := g.GetConditionalApproveInfoByKey(data.CreateIdxKey); condApproveInfo != nil {
-				if err := g.ProcessConditionalApprove(data, condApproveInfo, isInvalid); err != nil {
-					log.Printf("process conditional approve move failed: %s", err)
+		for {
+			data := dataIn.(*model.InscriptionBRC20Data)
+
+			// update latest height
+			g.BestHeight = data.Height
+
+
+			// is sending transfer
+			if data.IsTransfer {
+				// module conditional approve
+				if condApproveInfo, isInvalid := g.GetConditionalApproveInfoByKey(data.CreateIdxKey); condApproveInfo != nil {
+					if err := g.ProcessConditionalApprove(data, condApproveInfo, isInvalid); err != nil {
+						log.Printf("process conditional approve move failed: %s", err)
+					}
+					break
 				}
-				continue
+
+				// not first move
+				if data.Sequence != 1 {
+					break
+				}
+
+				// transfer
+				if transferInfo, isInvalid := g.GetTransferInfoByKey(data.CreateIdxKey); transferInfo != nil {
+					if err := g.ProcessTransfer(data, transferInfo, isInvalid); err != nil {
+						log.Printf("process transfer move failed: %s", err)
+					}
+					break
+				}
+
+				// module approve
+				if approveInfo, isInvalid := g.GetApproveInfoByKey(data.CreateIdxKey); approveInfo != nil {
+					if err := g.ProcessApprove(data, approveInfo, isInvalid); err != nil {
+						log.Printf("process approve move failed: %s", err)
+					}
+					break
+				}
+
+				// module commit
+				if commitFrom, isInvalid := g.GetCommitInfoByKey(data.CreateIdxKey); commitFrom != nil {
+					if err := g.ProcessCommit(commitFrom, data, isInvalid); err != nil {
+						log.Printf("process commit move failed: %s", err)
+					}
+					break
+				}
+
+				break
 			}
 
-			// not first move
-			if data.Sequence != 1 {
-				continue
+			// inscribe as fee
+			if data.Satoshi == 0 {
+				break
 			}
 
-			// transfer
-			if transferInfo, isInvalid := g.GetTransferInfoByKey(data.CreateIdxKey); transferInfo != nil {
-				if err := g.ProcessTransfer(data, transferInfo, isInvalid); err != nil {
-					log.Printf("process transfer move failed: %s", err)
-				}
-				continue
+			if ok := isJson(data.ContentBody); !ok {
+				// log.Println("not json")
+				break
 			}
 
-			// module approve
-			if approveInfo, isInvalid := g.GetApproveInfoByKey(data.CreateIdxKey); approveInfo != nil {
-				if err := g.ProcessApprove(data, approveInfo, isInvalid); err != nil {
-					log.Printf("process approve move failed: %s", err)
-				}
-				continue
+			// protocal, lower case only
+			body := new(model.InscriptionBRC20ProtocalContent)
+			if err := body.Unmarshal(data.ContentBody); err != nil {
+				// log.Println("Unmarshal failed", err, string(data.ContentBody))
+				break
 			}
 
-			// module commit
-			if commitFrom, isInvalid := g.GetCommitInfoByKey(data.CreateIdxKey); commitFrom != nil {
-				if err := g.ProcessCommit(commitFrom, data, isInvalid); err != nil {
-					log.Printf("process commit move failed: %s", err)
-				}
-				continue
+			// is inscribe deploy/mint/transfer
+			if body.Proto != constant.BRC20_P &&
+				body.Proto != constant.BRC20_P_MODULE &&
+				body.Proto != constant.BRC20_P_SWAP {
+				// log.Println("not proto")
+				break
 			}
 
-			continue
-		}
-
-		// inscribe as fee
-		if data.Satoshi == 0 {
-			continue
-		}
-
-		if ok := isJson(data.ContentBody); !ok {
-			// log.Println("not json")
-			continue
-		}
-
-		// protocal, lower case only
-		body := new(model.InscriptionBRC20ProtocalContent)
-		if err := body.Unmarshal(data.ContentBody); err != nil {
-			// log.Println("Unmarshal failed", err, string(data.ContentBody))
-			continue
-		}
-
-		// is inscribe deploy/mint/transfer
-		if body.Proto != constant.BRC20_P &&
-			body.Proto != constant.BRC20_P_MODULE &&
-			body.Proto != constant.BRC20_P_SWAP {
-			// log.Println("not proto")
-			continue
-		}
-
-		var process func(*model.InscriptionBRC20Data) error
-		if body.Proto == constant.BRC20_P && body.Operation == constant.BRC20_OP_DEPLOY {
-			process = g.ProcessDeploy
-		} else if body.Proto == constant.BRC20_P && body.Operation == constant.BRC20_OP_MINT {
-			process = g.ProcessMint
-		} else if body.Proto == constant.BRC20_P && body.Operation == constant.BRC20_OP_TRANSFER {
-			process = g.ProcessInscribeTransfer
-		} else if body.Proto == constant.BRC20_P_MODULE && body.Operation == constant.BRC20_OP_MODULE_DEPLOY {
-			process = g.ProcessCreateModule
-		} else if body.Proto == constant.BRC20_P_MODULE && body.Operation == constant.BRC20_OP_MODULE_WITHDRAW {
-			process = g.ProcessInscribeWithdraw
-		} else if body.Proto == constant.BRC20_P_SWAP && body.Operation == constant.BRC20_OP_SWAP_APPROVE {
-			process = g.ProcessInscribeApprove
-		} else if body.Proto == constant.BRC20_P_SWAP && body.Operation == constant.BRC20_OP_SWAP_CONDITIONAL_APPROVE {
-			process = g.ProcessInscribeConditionalApprove
-		} else if body.Proto == constant.BRC20_P_SWAP && body.Operation == constant.BRC20_OP_SWAP_COMMIT {
-			process = g.ProcessInscribeCommit
-		} else {
-			continue
-		}
-
-		if err := process(data); err != nil {
-			if body.Operation == constant.BRC20_OP_MINT {
-				if conf.DEBUG {
-					log.Printf("(%d %%) process failed: %s", 0, err)
-				}
+			var process func(*model.InscriptionBRC20Data) error
+			if body.Proto == constant.BRC20_P && body.Operation == constant.BRC20_OP_DEPLOY {
+				process = g.ProcessDeploy
+			} else if body.Proto == constant.BRC20_P && body.Operation == constant.BRC20_OP_MINT {
+				process = g.ProcessMint
+			} else if body.Proto == constant.BRC20_P && body.Operation == constant.BRC20_OP_TRANSFER {
+				process = g.ProcessInscribeTransfer
+			} else if body.Proto == constant.BRC20_P_MODULE && body.Operation == constant.BRC20_OP_MODULE_DEPLOY {
+				process = g.ProcessCreateModule
+			} else if body.Proto == constant.BRC20_P_MODULE && body.Operation == constant.BRC20_OP_MODULE_WITHDRAW {
+				process = g.ProcessInscribeWithdraw
+			} else if body.Proto == constant.BRC20_P_SWAP && body.Operation == constant.BRC20_OP_SWAP_APPROVE {
+				process = g.ProcessInscribeApprove
+			} else if body.Proto == constant.BRC20_P_SWAP && body.Operation == constant.BRC20_OP_SWAP_CONDITIONAL_APPROVE {
+				process = g.ProcessInscribeConditionalApprove
+			} else if body.Proto == constant.BRC20_P_SWAP && body.Operation == constant.BRC20_OP_SWAP_COMMIT {
+				process = g.ProcessInscribeCommit
 			} else {
-				log.Printf("(%d %%) process failed: %s", 0, err)
+				break
 			}
+
+			if err := process(data); err != nil {
+				if body.Operation == constant.BRC20_OP_MINT {
+					if conf.DEBUG {
+						log.Printf("(%d) process failed: %s", g.BestHeight, err)
+					}
+				} else {
+					log.Printf("(%d) process failed: %s", g.BestHeight, err)
+				}
+			}
+			break
+		}
+		if brc20DatasDump != nil {
+			brc20DatasDump <- dataIn
 		}
 	}
 
