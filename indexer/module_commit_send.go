ea4d5bc4ef56b8ac79c5dcb112963a637679176d
jiedo <jiedohh@gmail.com>
2024-05-16 19:45:39 +0800

    refactor: rename send param var

--- indexer/module_commit_send.go
+++ indexer/module_commit_send.go
@@ -13,19 +13,19 @@ func (g *BRC20ModuleIndexer) ProcessCommitFunctionSend(moduleInfo *model.BRC20Mo
 	addressTo := f.Params[0]
 	pkScriptTo, _ := utils.GetPkScriptByAddress(addressTo, conf.GlobalNetParams)
 
-	tokenOrPair := f.Params[1]
+	token := f.Params[1]
 	tokenAmtStr := f.Params[2]
 
-	tokenAmt, _ := g.CheckTickVerify(tokenOrPair, tokenAmtStr)
-	tokenBalanceFrom := moduleInfo.GetUserTokenBalance(tokenOrPair, f.PkScript)
+	tokenAmt, _ := g.CheckTickVerify(token, tokenAmtStr)
+	tokenBalanceFrom := moduleInfo.GetUserTokenBalance(token, f.PkScript)
 
 	// fixme: Must use the confirmed amount
 	if tokenBalanceFrom.SwapAccountBalance.Cmp(tokenAmt) < 0 {
-		log.Printf("token[%s] user[%s], balance %s", tokenOrPair, f.Address, tokenBalanceFrom)
+		log.Printf("token[%s] user[%s], balance %s", token, f.Address, tokenBalanceFrom)
 		return errors.New("send: token balance insufficient")
 	}
 
-	tokenBalanceTo := moduleInfo.GetUserTokenBalance(tokenOrPair, string(pkScriptTo))
+	tokenBalanceTo := moduleInfo.GetUserTokenBalance(token, string(pkScriptTo))
 
 	// User Real-time Balance Update
 	tokenBalanceFrom.SwapAccountBalance = tokenBalanceFrom.SwapAccountBalance.Sub(tokenAmt)
@@ -34,7 +34,7 @@ func (g *BRC20ModuleIndexer) ProcessCommitFunctionSend(moduleInfo *model.BRC20Mo
 	tokenBalanceFrom.UpdateHeight = g.BestHeight
 	tokenBalanceTo.UpdateHeight = g.BestHeight
 
-	log.Printf("pool send [%s] swappable: %s -> %s", tokenOrPair, tokenBalanceFrom.SwapAccountBalance, tokenBalanceTo.SwapAccountBalance)
+	log.Printf("pool send [%s] swappable: %s -> %s", token, tokenBalanceFrom.SwapAccountBalance, tokenBalanceTo.SwapAccountBalance)
 
 	return nil
 }
